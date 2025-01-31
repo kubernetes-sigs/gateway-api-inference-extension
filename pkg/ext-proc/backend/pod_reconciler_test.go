beac309d2b974f4fddd24d3bec1f3389f838b021
Kellen Swain <kfswain@google.com>
2025-01-31 20:40:02 +0000

    adding ready check and unit tests

--- pkg/ext-proc/backend/pod_reconciler_test.go
+++ pkg/ext-proc/backend/pod_reconciler_test.go
@@ -8,10 +8,14 @@ import (
 =======
 	"inference.networking.x-k8s.io/gateway-api-inference-extension/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+<<<<<<< HEAD
 >>>>>>> d5d500b (reversion to pod reconciliation):pkg/ext-proc/backend/pod_reconciler_test.go
 	v1 "k8s.io/api/core/v1"
 	discoveryv1 "k8s.io/api/discovery/v1"
 	"sigs.k8s.io/gateway-api-inference-extension/api/v1alpha1"
+=======
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+>>>>>>> 8042d21 (adding ready check and unit tests)
 )
 
 var (
@@ -34,6 +38,17 @@ func TestUpdateDatastore_EndpointSliceReconciler(t *testing.T) {
 				inferencePool: &v1alpha1.InferencePool{
 					Spec: v1alpha1.InferencePoolSpec{
 						TargetPortNumber: int32(8000),
+						Selector: map[v1alpha1.LabelKey]v1alpha1.LabelValue{
+							"some-key": "some-val",
+						},
+					},
+				},
+			},
+			incomingPod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "pod3",
+					Labels: map[string]string{
+						"some-key": "some-val",
 					},
 				},
 			},
@@ -49,10 +64,26 @@ func TestUpdateDatastore_EndpointSliceReconciler(t *testing.T) {
 					},
 				},
 			},
+			incomingPod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "pod3",
+					Labels: map[string]string{
+						"some-key": "some-val",
+					},
+				},
+				Status: corev1.PodStatus{
+					Conditions: []corev1.PodCondition{
+						{
+							Type:   corev1.PodReady,
+							Status: corev1.ConditionFalse,
+						},
+					},
+				},
+			},
 			wantPods: populateMap(basePod1, basePod2),
 		},
 		{
-			name: "Existing pod not ready, new pod added, and is ready",
+			name: "Existing pod not ready, remove",
 			datastore: &K8sDatastore{
 				pods: populateMap(basePod1, basePod2),
 				inferencePool: &v1alpha1.InferencePool{
@@ -61,47 +92,29 @@ func TestUpdateDatastore_EndpointSliceReconciler(t *testing.T) {
 					},
 				},
 			},
-			incomingSlice: &discoveryv1.EndpointSlice{
-				Endpoints: []discoveryv1.Endpoint{
-					{
-						TargetRef: &v1.ObjectReference{
-							Name: "pod1",
-						},
-						Zone: new(string),
-						Conditions: discoveryv1.EndpointConditions{
-							Ready: new(bool),
-						},
-						Addresses: []string{"0.0.0.0"},
+			incomingPod: &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "pod1",
+					Labels: map[string]string{
+						"some-key": "some-val",
 					},
-					{
-						TargetRef: &v1.ObjectReference{
-							Name: "pod2",
+				},
+				Status: corev1.PodStatus{
+					Conditions: []corev1.PodCondition{
+						{
+							Type:   corev1.PodReady,
+							Status: corev1.ConditionFalse,
 						},
-						Zone: new(string),
-						Conditions: discoveryv1.EndpointConditions{
-							Ready: truePointer(),
-						},
-						Addresses: []string{"0.0.0.0"},
-					},
-					{
-						TargetRef: &v1.ObjectReference{
-							Name: "pod3",
-						},
-						Zone: new(string),
-						Conditions: discoveryv1.EndpointConditions{
-							Ready: truePointer(),
-						},
-						Addresses: []string{"0.0.0.0"},
 					},
 				},
 			},
-			wantPods: populateMap(basePod3, basePod2),
+			wantPods: populateMap(basePod2),
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			endpointSliceReconciler := &EndpointSliceReconciler{Datastore: test.datastore, Zone: ""}
-			endpointSliceReconciler.updateDatastore(test.incomingSlice, test.datastore.inferencePool)
+			endpointSliceReconciler := &PodReconciler{Datastore: test.datastore}
+			endpointSliceReconciler.updateDatastore(test.incomingPod, test.datastore.inferencePool)
 
 			if mapsEqual(endpointSliceReconciler.Datastore.pods, test.wantPods) {
 				t.Errorf("Unexpected output pod mismatch. \n Got %v \n Want: %v \n",
@@ -110,27 +123,6 @@ func TestUpdateDatastore_EndpointSliceReconciler(t *testing.T) {
 			}
 		})
 	}
-}
-
-func mapsEqual(map1, map2 *sync.Map) bool {
-	equal := true
-
-	map1.Range(func(k, v any) bool {
-		if _, ok := map2.Load(k); !ok {
-			equal = false
-			return false
-		}
-		return true
-	})
-	map2.Range(func(k, v any) bool {
-		if _, ok := map1.Load(k); !ok {
-			equal = false
-			return false
-		}
-		return true
-	})
-
-	return equal
 }
 
 func truePointer() *bool {
