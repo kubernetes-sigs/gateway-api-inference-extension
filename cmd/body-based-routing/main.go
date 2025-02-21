b182e2e6f30f01b4dd2ef2e40d4dd93ac446d11c
Rohit Ramkumar <rramkumar@google.com>
2025-02-21 17:58:20 +0000

    Add README

--- cmd/body-based-routing/main.go
+++ cmd/body-based-routing/main.go
@@ -23,12 +23,12 @@ import (
 	"log"
 
 	"sigs.k8s.io/gateway-api-inference-extension/pkg/body-based-routing/service"
-	eppb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	"github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	address := "0.0.0.0:8181"
+	address := ":8181"
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("Failed to listen on insecure port: %v", err)
@@ -36,7 +36,7 @@ func main() {
 	fmt.Printf("Starting server on address %s\n", address)
 
 	grpcServer := grpc.NewServer()
-	eppb.RegisterExternalProcessorServer(grpcServer, &service.GRPCCalloutService{})
+	v3.RegisterExternalProcessorServer(grpcServer, &service.GRPCCalloutService{})
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC on insecure port: %v", err)
