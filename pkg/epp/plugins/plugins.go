256efcd5e0849c0c71ad284586d5f99cb56195e6
Shmuel Kallner <kallner@il.ibm.com>
2025-06-29 16:33:40 +0300

    Added a helper function to get referenced plugins by type
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- pkg/epp/plugins/plugins.go
+++ pkg/epp/plugins/plugins.go
@@ -16,7 +16,10 @@ limitations under the License.
 
 package plugins
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 // Plugin defines the interface for a plugin.
 // This interface should be embedded in all plugins across the code.
@@ -49,4 +52,18 @@ type HandlePlugins interface {
 
 	// GetAllPluginsWithNames returns all of the known plugins with their names
 	GetAllPluginsWithNames() map[string]Plugin
+}
+
+func PluginByType[P Plugin](handle Handle, name string) (P, error) {
+	var zero P
+
+	rawPlugin := handle.Plugins().Plugin(name)
+	if rawPlugin == nil {
+		return zero, fmt.Errorf("there was no plugin with the name '%s' defined", name)
+	}
+	thePlugin, ok := rawPlugin.(P)
+	if !ok {
+		return zero, fmt.Errorf("the plugin with the name '%s' was not an instance of %T", name, zero)
+	}
+	return thePlugin, nil
 }
