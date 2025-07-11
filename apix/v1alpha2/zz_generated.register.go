87bbe4d7e696b03fd98e8fb81c3c6d5b628472a2
Xiyue Yu <xiyue@google.com>
2025-07-11 10:59:32 -0700

    fixed code generator error

--- apix/v1alpha2/zz_generated.register.go
+++ apix/v1alpha2/zz_generated.register.go
@@ -63,6 +63,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&InferenceModel{},
 		&InferenceModelList{},
+		&InferencePool{},
+		&InferencePoolList{},
 	)
 	// AddToGroupVersion allows the serialization of client types like ListOptions.
 	v1.AddToGroupVersion(scheme, SchemeGroupVersion)
