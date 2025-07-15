c0e2832ae940aa04a9b9152a61c33d0eaa8bf738
Xiyue Yu <xiyue@google.com>
2025-07-15 13:17:27 -0700

    fixed imports

--- pkg/epp/backend/metrics/pod_metrics_test.go
+++ pkg/epp/backend/metrics/pod_metrics_test.go
@@ -26,7 +26,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/gateway-api-inference-extension/api/v1"
+	v1 "sigs.k8s.io/gateway-api-inference-extension/api/v1"
 )
 
 var (
