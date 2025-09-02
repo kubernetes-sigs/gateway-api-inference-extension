5168979d24b36f5050880c17c48d48b892d8fe69
Shmuel Kallner <kallner@il.ibm.com>
2025-09-02 10:57:10 +0300

    Explot new test configuration structure
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -95,13 +95,13 @@ func (testConfig *TestConfig) CreateCli() {
 }
 
 // DeleteClusterResources deletes all cluster-scoped objects the tests typically create.
-func DeleteClusterResources(ctx context.Context, cli client.Client) error {
+func DeleteClusterResources(testConfig *TestConfig) error {
 	binding := &rbacv1.ClusterRoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "auth-reviewer-binding",
 		},
 	}
-	err := cli.Delete(ctx, binding, client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err := testConfig.K8sClient.Delete(testConfig.Context, binding, client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -110,7 +110,7 @@ func DeleteClusterResources(ctx context.Context, cli client.Client) error {
 			Name: "auth-reviewer",
 		},
 	}
-	err = cli.Delete(ctx, role, client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.Delete(testConfig.Context, role, client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -119,7 +119,7 @@ func DeleteClusterResources(ctx context.Context, cli client.Client) error {
 			Name: "inference-gateway-sa-metrics-reader-role-binding",
 		},
 	}
-	err = cli.Delete(ctx, metricsReaderBinding, client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.Delete(testConfig.Context, metricsReaderBinding, client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -128,7 +128,7 @@ func DeleteClusterResources(ctx context.Context, cli client.Client) error {
 			Name: "inference-gateway-metrics-reader",
 		},
 	}
-	err = cli.Delete(ctx, metricsReaderRole, client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.Delete(testConfig.Context, metricsReaderRole, client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -137,7 +137,7 @@ func DeleteClusterResources(ctx context.Context, cli client.Client) error {
 			Name: "inferenceobjectives.inference.networking.x-k8s.io",
 		},
 	}
-	err = cli.Delete(ctx, model, client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.Delete(testConfig.Context, model, client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -146,7 +146,7 @@ func DeleteClusterResources(ctx context.Context, cli client.Client) error {
 			Name: "inferencepools.inference.networking.x-k8s.io",
 		},
 	}
-	err = cli.Delete(ctx, pool, client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.Delete(testConfig.Context, pool, client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -155,49 +155,49 @@ func DeleteClusterResources(ctx context.Context, cli client.Client) error {
 
 // DeleteNamespacedResources deletes all namespace-scoped objects the tests typically create.
 // The given namespace will also be deleted if it's not "default".
-func DeleteNamespacedResources(ctx context.Context, cli client.Client, ns string) error {
-	if ns == "" {
+func DeleteNamespacedResources(testConfig *TestConfig) error {
+	if testConfig.NsName == "" {
 		return nil
 	}
-	err := cli.DeleteAllOf(ctx, &appsv1.Deployment{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err := testConfig.K8sClient.DeleteAllOf(testConfig.Context, &appsv1.Deployment{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &corev1.Service{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &corev1.Service{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &corev1.Pod{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &corev1.Pod{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &corev1.ConfigMap{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &corev1.ConfigMap{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &corev1.Secret{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &corev1.Secret{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &corev1.ServiceAccount{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &corev1.ServiceAccount{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &v1.InferencePool{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &v1.InferencePool{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	err = cli.DeleteAllOf(ctx, &v1alpha2.InferenceObjective{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err = testConfig.K8sClient.DeleteAllOf(testConfig.Context, &v1alpha2.InferenceObjective{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
-	if ns != "default" {
+	if testConfig.NsName != "default" {
 		ns := &corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: ns,
+				Name: testConfig.NsName,
 			},
 		}
-		if err := cli.Delete(ctx, ns, client.PropagationPolicy(metav1.DeletePropagationForeground)); err != nil && !apierrors.IsNotFound(err) {
+		if err := testConfig.K8sClient.Delete(testConfig.Context, ns, client.PropagationPolicy(metav1.DeletePropagationForeground)); err != nil && !apierrors.IsNotFound(err) {
 			return err
 		}
 	}
@@ -205,11 +205,11 @@ func DeleteNamespacedResources(ctx context.Context, cli client.Client, ns string
 }
 
 // DeleteInferenceObjectiveResources deletes all InferenceObjective objects in the given namespace.
-func DeleteInferenceObjectiveResources(ctx context.Context, cli client.Client, ns string) error {
-	if ns == "" {
+func DeleteInferenceObjectiveResources(testConfig *TestConfig) error {
+	if testConfig.NsName == "" {
 		return nil
 	}
-	err := cli.DeleteAllOf(ctx, &v1alpha2.InferenceObjective{}, client.InNamespace(ns), client.PropagationPolicy(metav1.DeletePropagationForeground))
+	err := testConfig.K8sClient.DeleteAllOf(testConfig.Context, &v1alpha2.InferenceObjective{}, client.InNamespace(testConfig.NsName), client.PropagationPolicy(metav1.DeletePropagationForeground))
 	if err != nil && !apierrors.IsNotFound(err) {
 		return err
 	}
@@ -217,7 +217,7 @@ func DeleteInferenceObjectiveResources(ctx context.Context, cli client.Client, n
 }
 
 // PodReady checks if the given Pod reports the "Ready" status condition before the given timeout.
-func PodReady(ctx context.Context, cli client.Client, pod *corev1.Pod, timeout, interval time.Duration) {
+func PodReady(testConfig *TestConfig, pod *corev1.Pod) {
 	ginkgo.By(fmt.Sprintf("Checking pod %s/%s status is: %s", pod.Namespace, pod.Name, corev1.PodReady))
 	conditions := []corev1.PodCondition{
 		{
@@ -225,13 +225,14 @@ func PodReady(ctx context.Context, cli client.Client, pod *corev1.Pod, timeout, 
 			Status: corev1.ConditionTrue,
 		},
 	}
-	gomega.Eventually(checkPodStatus, timeout, interval).WithArguments(ctx, cli, pod, conditions).Should(gomega.BeTrue())
+	gomega.Eventually(checkPodStatus, testConfig.ExistsTimeout, testConfig.Interval).
+		WithArguments(testConfig, pod, conditions).Should(gomega.BeTrue())
 }
 
 // checkPodStatus checks if the given Pod status matches the expected conditions.
-func checkPodStatus(ctx context.Context, cli client.Client, pod *corev1.Pod, conditions []corev1.PodCondition) (bool, error) {
+func checkPodStatus(testConfig *TestConfig, pod *corev1.Pod, conditions []corev1.PodCondition) (bool, error) {
 	var fetchedPod corev1.Pod
-	if err := cli.Get(ctx, types.NamespacedName{Namespace: pod.Namespace, Name: pod.Name}, &fetchedPod); err != nil {
+	if err := testConfig.K8sClient.Get(testConfig.Context, types.NamespacedName{Namespace: pod.Namespace, Name: pod.Name}, &fetchedPod); err != nil {
 		return false, err
 	}
 	found := 0
@@ -246,7 +247,7 @@ func checkPodStatus(ctx context.Context, cli client.Client, pod *corev1.Pod, con
 }
 
 // DeploymentAvailable checks if the given Deployment reports the "Available" status condition before the given timeout.
-func DeploymentAvailable(ctx context.Context, cli client.Client, deploy *appsv1.Deployment, timeout, interval time.Duration) {
+func DeploymentAvailable(testConfig *TestConfig, deploy *appsv1.Deployment) {
 	ginkgo.By(fmt.Sprintf("Checking if deployment %s/%s status is: %s", deploy.Namespace, deploy.Name, appsv1.DeploymentAvailable))
 	conditions := []appsv1.DeploymentCondition{
 		{
@@ -254,19 +255,21 @@ func DeploymentAvailable(ctx context.Context, cli client.Client, deploy *appsv1.
 			Status: corev1.ConditionTrue,
 		},
 	}
-	gomega.Eventually(checkDeploymentStatus, timeout, interval).WithArguments(ctx, cli, deploy, conditions).Should(gomega.BeTrue())
+	gomega.Eventually(checkDeploymentStatus, testConfig.ModelReadyTimeout, testConfig.Interval).
+		WithArguments(testConfig.Context, testConfig.K8sClient, deploy, conditions).
+		Should(gomega.BeTrue())
 }
 
 // DeploymentReadyReplicas checks if the given Deployment has at least `count` ready replicas before the given timeout.
-func DeploymentReadyReplicas(ctx context.Context, cli client.Client, deploy *appsv1.Deployment, count int, timeout, interval time.Duration) {
+func DeploymentReadyReplicas(testConfig *TestConfig, deploy *appsv1.Deployment, count int) {
 	ginkgo.By(fmt.Sprintf("Checking if deployment %s/%s has at least %d ready replica(s)", deploy.Namespace, deploy.Name, count))
 	gomega.Eventually(func(g gomega.Gomega) {
 		var fetchedDeploy appsv1.Deployment
-		err := cli.Get(ctx, types.NamespacedName{Namespace: deploy.Namespace, Name: deploy.Name}, &fetchedDeploy)
+		err := testConfig.K8sClient.Get(testConfig.Context, types.NamespacedName{Namespace: deploy.Namespace, Name: deploy.Name}, &fetchedDeploy)
 		g.Expect(err).NotTo(gomega.HaveOccurred())
 		g.Expect(fetchedDeploy.Status.ReadyReplicas).To(gomega.BeNumerically(">=", count),
 			fmt.Sprintf("Deployment only has %d ready replicas, want at least %d", fetchedDeploy.Status.ReadyReplicas, count))
-	}, timeout, interval).Should(gomega.Succeed())
+	}, testConfig.ModelReadyTimeout, testConfig.Interval).Should(gomega.Succeed())
 }
 
 // checkDeploymentStatus checks if the given Deployment status matches the expected conditions.
@@ -287,7 +290,7 @@ func checkDeploymentStatus(ctx context.Context, cli client.Client, deploy *appsv
 }
 
 // CRDEstablished checks if the given CRD reports the "Established" status condition before the given timeout.
-func CRDEstablished(ctx context.Context, cli client.Client, crd *apiextv1.CustomResourceDefinition, timeout, interval time.Duration) {
+func CRDEstablished(testConfig *TestConfig, crd *apiextv1.CustomResourceDefinition) {
 	ginkgo.By(fmt.Sprintf("Checking CRD %s status is: %s", crd.Name, apiextv1.Established))
 	conditions := []apiextv1.CustomResourceDefinitionCondition{
 		{
@@ -295,7 +298,9 @@ func CRDEstablished(ctx context.Context, cli client.Client, crd *apiextv1.Custom
 			Status: apiextv1.ConditionTrue,
 		},
 	}
-	gomega.Eventually(checkCrdStatus, timeout, interval).WithArguments(ctx, cli, crd, conditions).Should(gomega.BeTrue())
+	gomega.Eventually(checkCrdStatus, testConfig.ReadyTimeout, testConfig.Interval).
+		WithArguments(testConfig.Context, testConfig.K8sClient, crd, conditions).
+		Should(gomega.BeTrue())
 }
 
 // checkCrdStatus checks if the given CRD status matches the expected conditions.
@@ -321,22 +326,14 @@ func checkCrdStatus(
 }
 
 // ExecCommandInPod runs a command in a given container of a given Pod, returning combined stdout+stderr.
-func ExecCommandInPod(
-	ctx context.Context,
-	cfg *rest.Config,
-	scheme *runtime.Scheme,
-	kubeClient *kubernetes.Clientset,
-	podNamespace, podName, containerName string,
-	cmd []string,
-) (string, error) {
+func ExecCommandInPod(testConfig *TestConfig, podName, containerName string, cmd []string) (string, error) {
+	parameterCodec := runtime.NewParameterCodec(testConfig.Scheme)
 
-	parameterCodec := runtime.NewParameterCodec(scheme)
-
-	req := kubeClient.CoreV1().RESTClient().
+	req := testConfig.KubeCli.CoreV1().RESTClient().
 		Post().
 		Resource("pods").
 		Name(podName).
-		Namespace(podNamespace).
+		Namespace(testConfig.NsName).
 		SubResource("exec").
 		VersionedParams(&corev1.PodExecOptions{
 			Container: containerName,
@@ -347,13 +344,13 @@ func ExecCommandInPod(
 			TTY:       false,
 		}, parameterCodec)
 
-	exec, err := remotecommand.NewSPDYExecutor(cfg, "POST", req.URL())
+	exec, err := remotecommand.NewSPDYExecutor(testConfig.RestConfig, "POST", req.URL())
 	if err != nil {
 		return "", fmt.Errorf("could not initialize executor: %w", err)
 	}
 
 	var stdout, stderr bytes.Buffer
-	execErr := exec.StreamWithContext(ctx, remotecommand.StreamOptions{
+	execErr := exec.StreamWithContext(testConfig.Context, remotecommand.StreamOptions{
 		Stdout: &stdout,
 		Stderr: &stderr,
 	})
@@ -369,8 +366,10 @@ func ExecCommandInPod(
 
 // EventuallyExists checks if a Kubernetes resource exists and returns nil if successful.
 // It takes a function `getResource` which retrieves the resource and returns an error if it doesn't exist.
-func EventuallyExists(ctx context.Context, getResource func() error, timeout, interval time.Duration) {
+func EventuallyExists(testConfig *TestConfig, getResource func() error) {
 	gomega.Eventually(func() error {
 		return getResource()
-	}, timeout, interval).Should(gomega.Succeed())
+	}, testConfig.ExistsTimeout, testConfig.Interval).Should(gomega.Succeed())
+}
+
 }
