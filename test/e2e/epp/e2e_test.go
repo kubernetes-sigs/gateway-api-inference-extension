4e15a8fae6c799425e77128530e3f0b0f044fba0
Zhengke Zhou <madzhou1@gmail.com>
2025-09-18 19:16:39 +0800

    Update test/e2e/epp/e2e_test.go
    Co-authored-by: Nir Rozenbaum <nirro@il.ibm.com>

--- test/e2e/epp/e2e_test.go
+++ test/e2e/epp/e2e_test.go
@@ -213,7 +213,9 @@ func verifyTrafficRouting() {
 						},
 						{
 							"type": "image_url",
-							"url":  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+							"image_url":  {
+								"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+							},
 						},
 					},
 				},
